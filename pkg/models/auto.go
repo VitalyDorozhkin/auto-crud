730e57483680010c8d716b6bc9e1850c313f92fb
VitalyDorozhkin <dorozhkin.vitaly@gmail.com>
2020-07-16 01:11:41 +0300

    add request/response models

--- pkg/models/auto.go
+++ pkg/models/auto.go
@@ -8,3 +8,21 @@ type Auto struct {
 	Status  string `json:"status"`
 	Mileage int32  `json:"mileage"`
 }
+
+type AutoRequest struct {
+	Brand   string `json:"brand"`
+	Model   string `json:"model"`
+	Price   uint32 `json:"price"`
+	Status  string `json:"status"`
+	Mileage int32  `json:"mileage"`
+}
+
+type StatusResponse struct {
+	Data  *bool   `json:"data,omitempty"`
+	Error *string `json:"error,omitempty"`
+}
+
+type AutoResponse struct {
+	Data  *Auto   `json:"data,omitempty"`
+	Error *string `json:"error,omitempty"`
+}
